8c00b731eb52ab52752753ef692738c9f2f9e774
WrWl <100027250+WrWl@users.noreply.github.com>
2022-09-23 20:13:51 +0300

    /

--- src/index.js
+++ src/index.js
@@ -5,35 +5,11 @@ const input = document.querySelector(".input")
 const loadMoreBtn = document.querySelector(".load-more")
 const gallery = document.querySelector(".gallery")
 loadMoreBtn.style.visibility = 'hidden'
-const addMore = 1;
-function createImg(images) {
-    const markup = images.map((img) => {
-            return `<div class="gallery">
-        <div class="photo-card">
-            <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
-            <div class="info">
-                <p class="info-item">
-                    <b>${img.likes}</b>
-                </p>
-                <p class="info-item">
-                    <b>${img.views}</b>
-                </p>
-                <p class="info-item">
-                    <b>${img.comments}</b>
-                </p>
-                <p class="info-item">
-                    <b>${img.downloads}</b>
-                </p>
-            </div>
-        </div>
-    </div>`
-        })
-        .join("");
-    gallery.innerHTML = markup;
-}
-    
+let addMore = 1; 
+
+// звернення до серверу
 function getPhoto(name) {
-    return axios.get(`https://pixabay.com/api/`, {
+   return  axios.get(`https://pixabay.com/api/`, {
     params: {
         key: '30077933-3f2db5638137745ffc2686be7',
             q: name,
@@ -44,43 +20,73 @@ function getPhoto(name) {
         page: addMore
     }
 })
-    .then(response => {
-        return response.data
+        .then(response => {
+        
+        return response
     })
     
 }
 
+//функція, яка відрисовує картинки
+function createImg(images) {
+    
+    const markup = images
+        .map((img) => {
+            return `<div class="gallery">
+        <div class="photo-card">
+            <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
+            <div class="info">
+                <p class="info-item">
+                    <b>Likes: ${img.likes}</b>
+                </p>
+                <p class="info-item">
+                    <b>Views: ${img.views}</b>
+                </p>
+                <p class="info-item">
+                    <b>Comments: ${img.comments}</b>
+                </p>
+                <p class="info-item">
+                    <b>Downloads: ${img.downloads}</b>
+                </p>
+            </div>
+        </div>
+    </div>`
+        })
+        .join("");
+    gallery.innerHTML = markup;
+} 
+    // подія, при якій створюються картинки
 form.addEventListener("submit", photoName => {
     photoName.preventDefault();
-    photoname = input.value;
-    getPhoto(photoName)
-        .then(data => {
-            if (data.length = 0 ) {
+     a = input.value
+    getPhoto(a)
+        .then(res => {
+            console.log(res.data.hits)
+            if (res.data.hits.length == 0) {
                 Notify.faillure('Sorry, there are no images matching your search query. Please try again.')
-                gallery.innerHTML = '';
-                gallery.innerHTML = '';
-                return
+                
             }
             else {
-                createImg(data);
-                gallery.innerHTML = ''
-                loadMoreBtn.style.visibility ='visible'
+                createImg(res.data.hits)
+                loadMoreBtn.style.visibility = 'visible'
             }
-    })
+        })
 }) 
+//подія при якій додаються наступні картинки
 loadMoreBtn.addEventListener("click", photoName => {
     photoName.preventDefault()
-    photoName = input.value;
-    getPhoto(photoName)
-        .then(data => {
-            if (data.length == totalHits) {
+    
+    getPhoto(a)
+        .then(res => {
+            if (res.data.hits == res.totalHits) {
             Notify.info("We're sorry, but you've reached the end of search results.")
             }
             else {
                 addMore++;
-                createImg(data);
-                gallery.innerHTML = ''
+                createImg(res.data.hits);
+                
             }
     })
+    
 
-} )
+} ) 
